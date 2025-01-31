f182b4ca58b7ad0247a2c73fc19e245553173fb9
UrielMarles <marlesurielnicolas@gmail.com>
2025-01-30 21:27:02 -0300

    cosas ejemplo

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -1,10 +1,16 @@
 // app/home/page.tsx
-import Image from 'next/image';
-import styles from './home.module.css';
-import '../../styles/globals.css';
 
+import '../../styles/globals.css';
+import LoginForm from '@/components/testMaterial/testMaterial';
 export const metadata = {
   title: 'Inicio',
   description: 'Página de inicio de la aplicación',
 };
 
+export default function Home() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
