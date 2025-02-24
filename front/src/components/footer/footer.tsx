b814a4a9a7504b65b98c008cadf8c0db3d86c1f2
GaboFW <gabibo.fewars@gmail.com>
2025-02-23 21:33:46 -0300

    commit

--- front/src/components/footer/footer.tsx
+++ front/src/components/footer/footer.tsx
@@ -5,7 +5,7 @@ import styles from "./footer.module.css";
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <p>© 2025 Diamango</p>
+            <p>© 2025 Dinamango</p>
         </footer>
     );
 }
